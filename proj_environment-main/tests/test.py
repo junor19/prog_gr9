737b4bc8ae64bfe0c981b800ff4156c7fada4481
helenebakke <helene.bakke@gmail.com>
2025-03-30 10:02:12 +0200

    små forrandring

--- proj_environment-main/tests/test.py
+++ proj_environment-main/tests/test.py
@@ -8,10 +8,10 @@ import hente_apidata
 
 class testmonth(unittest.TestCase):
     def test_date(self):
-        #with open(raa_data_d.csv, 'r') as csvfile:
-         #   reader = csv.reader(csvfile)
-          #  for row in reader:
-           #     date = datetime.strptime(date_string,"2020-01-01")
+        with open(raa_data_d.csv, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                date = datetime.strptime(date_string,"2020-01-01")
         year = 
         forventet_dato = datetime(2020,10,10)
         self.assertIn(forventet_dato, hente_apidata)
