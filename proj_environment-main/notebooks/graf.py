e40ea49e3d6ca1734369da1216274ac7c83994ac
helenebakke <helene.bakke@gmail.com>
2025-04-22 15:00:58 +0200

    ukomplett

---
+++ proj_environment-main/notebooks/graf.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import sys
+import os
+
+src_path=sys.path.append(os.path.join(os.path.dirname(__file__),'../src'))
+sys.path.append(src_path)
+
+import hente_apidata
+import variabler
+
+fig, axs = plt.subplots(1,3, figsize=(9,3), sharey=True)
+
+axs[0].bar(seksten)
+axs[1].scatter(t_månden)
+axs[2].plot(t_dager,seksten)
+fig.suptitle('solskinnstimer')
---
+++ proj_environment-main/notebooks/median_osv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'../src'))
+
+import hente_apidata
+
+hente_apidata.hent_solskinnstimer("t_månder","t_dager", "raa_data_m.csv")
+
+df =pd.read_csv(os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_m.csv'))
+
+print(df.median()) #summerer median
+
+print(df.mean()) #summerer gjennomsnitt
+
+print(df.std()) # finner standardavviik
