ad5d4d9f2f4b3e8598d94c8b7f974cc612efb73c
helenebakke <helene.bakke@gmail.com>
2025-05-02 13:15:21 +0200

    median og gjennomsnitt ferdig

--- proj_environment-main/src/median_osv.py
+++ proj_environment-main/src/median_osv.py
@@ -3,15 +3,22 @@ import numpy as np
 import sys
 import os
 
-
 import hente_apidata
 
-hente_apidata.hent_solskinnstimer("t_månder","t_dager", "raa_data_m.csv")
+hente_apidata.hent_solskinnstimer("t_månder","seksten", "raa_data_m.csv")
+hente_apidata.hent_solskinnstimer("t_månder", "tjue", "raa_data_d.csv")
 
-df =pd.read_csv(os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_m.csv'))
+df_m =pd.read_csv(os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_m.csv'))
+df_d =pd.read_csv(os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_d.csv'))
 
-print(df.median()) #summerer median
+print("median er:", df_m.median(numeric_only=True))#summerer median
 
-print(df.mean()) #summerer gjennomsnitt
+print("gjennomsnittet er:",df_m.mean(numeric_only=True))#summerer gjennomsnitt
 
-print(df.std()) # finner standardavviik
+print("standardavviket er:",df_m.std(numeric_only=True))# finner standardavviik
+
+print("median er:", df_d.median(numeric_only=True)) #summerer median
+
+print("gjennomsnittet er:", df_d.mean(numeric_only=True)) # summerergjennomsnittet
+
+print("standardavviket er:", df_d.std(numeric_only=True)) #finner standardavviket
