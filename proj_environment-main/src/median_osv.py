206bde933401b7bd90f4ccf195b99a7bd5d96b7e
helenebakke <helene.bakke@gmail.com>
2025-05-23 12:22:09 +0200

    median gjort til funksjon

--- proj_environment-main/src/median_osv.py
+++ proj_environment-main/src/median_osv.py
@@ -3,15 +3,20 @@ import numpy as np
 import sys
 import os
 
-import hente_apidata
+def statistikk(filnavn):
+    df_d =pd.read_csv(filnavn)
+    median = df_d.median(numeric_only=True)
+    gjennomsnitt = df_d.mean(numeric_only=True)
+    standardavvik = df_d.std(numeric_only=True)
+    return median, gjennomsnitt, standardavvik
 
-hente_apidata.hent_solskinnstimer("t_månder", "tjue", "raa_data_d.csv")
+if __name__ == "__main__":
+    import hente_apidata
+    hente_apidata.hent_solskinnstimer("t_månder", "tjue", "raa_data_d.csv")
+    
+    filbane = os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_d.csv')
+    median, gjennomsnitt, standardavvik = statistikk(filbane)
 
-df_d =pd.read_csv(os.path.join(os.path.dirname(__file__),'..', 'data', 'raa_data_d.csv'))
-
-
-print("median er:", df_d.median(numeric_only=True)) #summerer median
-
-print("gjennomsnittet er:", df_d.mean(numeric_only=True)) # summerergjennomsnittet
-
-print("standardavviket er:", df_d.std(numeric_only=True)) #finner standardavviket
+print("median er:",median) #summerer median
+print("gjennomsnittet er:", gjennomsnitt) # summerergjennomsnittet
+print("standardavviket er:", standardavvik) #finner standardavviket
