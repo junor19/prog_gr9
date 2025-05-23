349df4f1a91fbb6bf218f5282bb89b640d8e1a5c
junor19 <juneor@stud.ntnu.no>
2025-05-23 13:09:08 +0200

    lagt inn teori og forklaring av CSV-fil

--- proj_environment-main/src/variabler.py
+++ proj_environment-main/src/variabler.py
@@ -1,15 +1,29 @@
 #API-nøkkel som bør ligge i et annet doc
 client_id = "bd3f25c0-324e-4a26-a507-5887be8b3c9d"
 
+''' fra 
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the environment variables
+api_key = os.getenv('API_KEY')
+
+if api_key is None:
+    raise ValueError("API key not found in environment variables.")
+    
+'''
+
 #variablene
 stasjon_id = "SN68173"  #stasjons-id for gløs
 
 elementer = {
-    "t_månder": "sum(duration_of_sunshine P1M)",  #hvor mange timer med sol det har vært i den måneden.
     "t_dager": "sum(duration_of_sunshine P1D)",  #hvor mange timer med sol det har vært i døgnet.
 }
     
 tidsperioder = {
-    "seksten": "2016-01-01/2021-01-01",  #for stasjon gløshaugen var det tilgjengelig data fra og med 2016, så det er der vi starter
-    "tjue": "2020-01-01/2021-01-01",  #for å få et sett ukomplett data har vi også hentet inn data fra 2020-2021 men i timer/døgnet ikke timer/månden
+    "seksten": "2016-01-01/2021-01-01",  #for stasjon gløshaugen var det tilgjengelig data fra og med 2016, så det er der vi startet
 }
