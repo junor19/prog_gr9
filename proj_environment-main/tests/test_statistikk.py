2426356bda93e6ff8da3b82ddf3fbd056eba4909
helenebakke <helene.bakke@gmail.com>
2025-05-23 14:25:50 +0200

    justeringer av testene

--- proj_environment-main/tests/test_statistikk.py
+++ proj_environment-main/tests/test_statistikk.py
@@ -9,10 +9,30 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from median_osv import statistikk
 
 class test_statistikk(unittest.TestCase):
-    def median_test(self):
-        forventet_median, _, _ = statistikk(self.test_fil)
-        ikke_rett_median = 5
-        self.assertNotEqual(forventet_median,ikke_rett_median)
+    def setUp(self):
+        self.filnavn = 'testdata.csv'
+        self.kolonnenavn = 'verdier'
+        self.data = pd.DataFrame({
+            self.kolonnenavn: [1, 3, 5, 7, 9]  # Median = 5, Gj.snitt = 5, Std.avvik ≈ 3.16
+        })
+        self.data.to_csv(self.filnavn, index=False)
 
+    def tearDown(self):
+        os.remove(self.filnavn)
+    
+    def test_median(self):
+        median, _, _ = statistikk(self.test_fil)
+        self.assertEqual(median[self.kolonnenavn], 5)
+        
+    def test_gjennomsnitt(self):
+        _, gjennomsnitt, _ = statistikk(self.test_fil)
+        self.assertNotEqual(gjennomsnitt[self.kolonnenavn], 9)
+        
+    def test_standardavvik(self):
+        _, _, standardavvik = statistikk(self.test_fil)
+        self.assertNotEqual(standardavvik[self.kolonnenavn], 0)
+        
+    
+        
 if __name__ == "__main__":
     unittest.main()
